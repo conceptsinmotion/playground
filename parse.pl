3c4cc083f085bc021dd51d322c175e639c353061
Jan Wielemaker <J.Wielemaker@cs.vu.nl>
2019-08-23 11:44:12 +0200

    Simple experiment

--- parse.pl
+++ parse.pl
@@ -2,4 +2,23 @@
 :- use_module(library(thea2/owl2_manchester_parser)).
 
 load :-
-    owl_parse_manchester_syntax_file('../ontoTextProv/manchester.owl').
+    retract_all_axioms,
+    owl2_model_init,
+    catch(owl_parse_manchester_syntax_file('../ontoTextProv/manchester.owl'),
+          E,
+          (   print_message(error, E),
+              fail
+          )).
+
+validate :-
+    forall(axiom(X), validate(X)).
+
+validate(X) :-
+    valid_axiom(X),
+    !.
+validate(X) :-
+    format(user_error, 'Invalid: ~p~n', [X]).
+
+
+l :-
+    owl_parse_manchester_syntax_file('test1.owl').
